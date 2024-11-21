b9f5e05613b761ca0de4a2a9303673010d9ca2e1
Victor Hugo <victorhmds@hotmail.com>
2024-11-21 16:50:37 -0300

    second commit

--- square.py
+++ square.py
@@ -1,6 +1,26 @@
-x = 4
-s = pow(x,2)
-print(s)
+import math
 
-l = list(range(1,s+1))
-print(l)
+def check_square_number(x):
+    x = math.sqrt(x)
+    return x.is_integer()
+
+def make_numbers_list(x):
+    return list(range(1,x+1))
+
+def get_interval(x):
+    return int(math.sqrt(x))
+
+x = 9
+is_square = check_square_number(x)
+
+if is_square:
+    print("O número inserido é um número quadrado")
+else:
+    print("O número inserido não é um número quadrado")
+
+interval = get_interval(x)
+
+list = make_numbers_list(x)
+
+for number in list:
+    print(number)
