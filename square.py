aac12b400791b58e7402c99d5acfdf4966e18b5e
Victor Hugo <victorhmds@hotmail.com>
2024-11-21 20:10:45 -0300

    third commit

--- square.py
+++ square.py
@@ -1,4 +1,8 @@
 import math
+from prettytable import PrettyTable
+
+def ask_number():
+    return int(input("Insert a square number: "))
 
 def check_square_number(x):
     x = math.sqrt(x)
@@ -10,17 +14,23 @@ def make_numbers_list(x):
 def get_interval(x):
     return int(math.sqrt(x))
 
-x = 9
+x = ask_number()
 is_square = check_square_number(x)
 
 if is_square:
-    print("O número inserido é um número quadrado")
+    print("The number is a square number")
 else:
-    print("O número inserido não é um número quadrado")
+    print("The number isn't a square number")
 
 interval = get_interval(x)
 
-list = make_numbers_list(x)
+unformatted_list = make_numbers_list(x)
+formatted_list = list()
 
-for number in list:
-    print(number)
+for i in range(0, len(unformatted_list), interval):
+    formatted_list.append(unformatted_list[i:i+interval])
+
+table = PrettyTable()
+table.add_rows(formatted_list)
+table.hrules = True
+print(table)
